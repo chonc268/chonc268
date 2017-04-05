da80c1067cc16923d6427327c2c60be1cf86568c
chonc268 <chonc268@newschool.edu>
2017-04-05 19:46:18 -0400

    adding Quiz

---
+++ projects/main.js
@@ -0,0 +1,46 @@
+console.log('Carol Chong');
+console.log(9);
+console.log('9');
+console.log(true);
+console.log(['C','A','R','O','L']);
+console.log (['boolean', 'array', 'string']);
+console.log('Carol'+'Chong');
+console.log(50+50);
+
+var myArray = ['C','A','R','O','L'];
+console.log(firstLetter = myArray[myArray.length - 5]);
+console.log(lastLetter = myArray[myArray.length - 1]);
+
+
+var characters = 'supercalifragilisticexpialidocious';
+console.log(numberCharacters = characters.length);
+
+var myFirst= ['C','A','R','O','L'];
+var myLast= ['C','H','O','N','G']
+console.log(myFirst.length == myLast.length)
+
+
+var students = [
+  'Carol Chong',
+  'Gaurika Milan Dalal',
+  'Vitoria Feitosa Di Iulio',
+  'Nafisa Afrin Iqbal',
+  'Moonnyung Jo',
+  'Ana Sofia Murillo',
+  'Gyuri Nam',
+  'Jissaura Taveras Hernandez',
+  'Elizabeth Tsai',
+  'Kyle Yu'
+];
+if ( students.length == 10 ){
+  console.log("all students are in class");
+} else if (students.length < 9) {
+  console.log('not everyone is here')
+} else {
+  console.log ('not everyone is here')
+}
+
+var characterFirst= ['C','A','R','O','L'];
+for ( var i = 0; i < characterFirst.length; i++) {
+  console.log( characterFirst[i] + ' ' + i);
+}
