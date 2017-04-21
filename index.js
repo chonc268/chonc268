7b2a8f16f3228bfb6cf12a29d8ee7060a476fdda
chonc268 <chonc268@newschool.edu>
2017-04-21 05:45:36 -0400

    adding website again

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 $(function() {
-  $(".button").click(function(){
-    $("p").toggle();
+  $('.hello').click(function() {
+    if ( $('body').hasClass('active') ) {
+      $('body').removeClass('active').css('background', '');
+    } else {
+      $('body').addClass('active').css('background', '#FFD100');
+    }
   });
 });
-
