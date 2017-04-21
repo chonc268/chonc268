eea5785b8ff51d3af90d544f67be88903c8e77a0
chonc268 <chonc268@newschool.edu>
2017-04-21 05:48:25 -0400

    trial

--- home_page/main.js
+++
@@ -1,9 +0,0 @@
-$(function() {
-  $('.hello').click(function() {
-    if ( $('body').hasClass('active') ) {
-      $('body').removeClass('active').css('background', '');
-    } else {
-      $('body').addClass('active').css('background', '#FFD100');
-    }
-  });
-});
