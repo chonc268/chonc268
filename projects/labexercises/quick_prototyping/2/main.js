d95f5ff66214fd0591c5d4c7362c54f272b7f1b5
chonc268 <chonc268@newschool.edu>
2017-04-26 20:40:48 -0400

    adding prototype 2

--- projects/labexercises/quick_prototyping/2/main.js
+++ projects/labexercises/quick_prototyping/2/main.js
@@ -1,3 +1,40 @@
+var date = new Date();
+
+setInterval(function(){
+  rotateGridObjects();
+  //console.log('function is running');
+}, 1);
+drawGridObjects();
+
+function updateTime() {
+  date = new Date();
+  $('.time-gmt').html( date.toGMTString() );
+  $('.time-seconds').html( date.getTime() );
+}
+
 $(function() {
-  // Your interactions go here
+  updateTime();
+  setInterval(updateTime, 50);
+
+  $('.month').html( date.getMonth() );
+  $('.day').html( date.getDate() );
+  $('.year').html( date.getUTCFullYear() );
 });
+
+function rotateGridObjects(){
+  var now = new Date();
+  var second = now.getSeconds();
+  //console.log(second);
+  $('.random').css('transform','rotate(' + second*10 + 'deg)');
+}
+
+function drawGridObjects(){
+  for (j = 0; j < 1; j++) {
+    for (i = 0; i < 14; i++) {
+      $("body").append( "<div class='element' style='left:" + i*7 + "vw; top:" + j*100 + "px;'></div>" );
+      // console.log("element " + j + ", " + i + " added");
+    }
+  }
+}
+
+
