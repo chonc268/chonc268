0a43f61201c1193a8c60f6683fb097f836e68e9c
chonc268 <chonc268@newschool.edu>
2019-04-07 23:46:15 -0400

    adding gif

--- projects/index_12/thesis_script_12.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("mousemove", function (event) {
-  const mouseX = event.pageX
-  const mouseY = event.pageY
-
-  const ball = document.querySelector("div.ball")
-
-  ball.style.left = mouseX + "px"
-  ball.style.top = mouseY + "px"
-})
